372404b87e6da6d01e113d1d9ac1a7b977ea3828
bschetzsle <bschetzs@uci.edu>
2022-03-16 12:45:20 -0700

    added some simulations in Poisson

--- poisson_example.R
+++ poisson_example.R
@@ -21,6 +21,7 @@ prior_log_density <- function(theta)
 
 true_lambda <- 30
 
+set.seed(100)
 y = rpois(100, true_lambda)
 
 result <- mcmc_bsl(
@@ -50,26 +51,67 @@ p1 <- ggplot(cbind(temp$x, temp$y), aes(x, y)) +
 
 
 result <- mcmc_bsl(
-  y                 = rpois(100, true_lambda),
+  y                 = y,
   prior_log_density = prior_log_density,
   rand_likelihood   = function(n, theta) rpois(n, theta$lambda),
   rand_proposal     = rand_proposal,
   statistic         = function(y) max(y),
-  initial_theta     = list(lambda = 6.29),
+  initial_theta     = list(lambda = 6),
   iterations = 1000
 )
 
-plot_param(result, lambda, true_lambda) / plot_log_prob(result)
+p = plot_param(result, lambda, true_lambda) / plot_log_prob(result)
+ggsave("./plots/Poisson_max.png", plot = p, width = 30, height = 20, units = "cm")
+
+#why are the estimates of lambda off? it turns out y had an unusually large max
+plot(density(replicate(1000, max(rpois(100,30)))))
+abline(v=max(y))
 
 
 result <- mcmc_bsl(
-  y                 = rpois(100, true_lambda),
+  y                 = y,
+  prior_log_density = prior_log_density,
+  rand_likelihood   = function(n, theta) rpois(n, theta$lambda),
+  rand_proposal     = rand_proposal,
+  statistic         = function(y) c(min(y), max(y)),
+  initial_theta     = list(lambda = 6),
+  iterations = 1000
+)
+
+p = plot_param(result, lambda, true_lambda) / plot_log_prob(result)
+ggsave("./plots/Poisson_min_max.png", plot = p, width = 30, height = 20, units = "cm")
+
+plot(density(replicate(1000, min(rpois(100,30)))))
+abline(v=min(y))
+plot(density(replicate(1000, max(rpois(100,30)))))
+abline(v=max(y))
+
+
+result <- mcmc_bsl(
+  y                 = y,
   prior_log_density = prior_log_density,
   rand_likelihood   = function(n, theta) rpois(n, theta$lambda),
   rand_proposal     = rand_proposal,
   statistic         = function(y) lm(sort(y) ~ poly(1:length(y), degree=3))$coef,
-  initial_theta     = list(lambda = 6.29),
+  initial_theta     = list(lambda = 6),
   iterations        = 1000
 )
 
-plot_param(result, lambda, true_lambda) / plot_log_prob(result)
+ggsave("./plots/Poisson_sorted_Y.png", plot(sort(y), ylab="Count", main="Sorted Observations"),
+       width = 30, height = 20, units = "cm")
+
+p = plot_param(result, lambda, true_lambda) / plot_log_prob(result)
+ggsave("./plots/Poisson_poly_regress.png", plot = p, width = 30, height = 20, units = "cm")
+
+plot(density(replicate(1000, min(rpois(100,30)))))
+
+temp = replicate(1000, lm(sort(rpois(100,30)) ~ poly(1:length(y), degree=3))$coef)
+
+plot(density(temp[,1]))
+abline(v=lm(sort(y) ~ poly(1:length(y), degree=3))$coef[1])
+plot(density(temp[,2]))
+abline(v=lm(sort(y) ~ poly(1:length(y), degree=3))$coef[2])
+plot(density(temp[,3]))
+abline(v=lm(sort(y) ~ poly(1:length(y), degree=3))$coef[3])
+plot(density(temp[,4]))
+abline(v=lm(sort(y) ~ poly(1:length(y), degree=3))$coef[4])
